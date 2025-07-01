876aa108a9bfe41e1f8e090c68e45eec50446c25
rafix12kingsatan <rafix1278@gmail.com>
2025-07-01 11:32:53 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'pl.dlaroslin.app',
   appName: 'dlaroslin.pl',
-  webDir: 'build'
+  webDir: 'build',
+  ios: {
+    path: 'ios'
+  }
 };
 
 export default config;
+
